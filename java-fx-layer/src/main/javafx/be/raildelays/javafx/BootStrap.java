6a6906f51edba6eae98e66dc497b787bdf3b1874
almex <soumagne.alexis@belgacom.net>
2012-02-20 21:39:46 +0100

    Added the JavaFX layer

---
+++ java-fx-layer/src/main/javafx/be/raildelays/javafx/BootStrap.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package be.raildelays.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * @author Almex
+ */
+public class BootStrap extends Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+    public void start(Stage primaryStage) {
+        
+        primaryStage.show();
+    }
+
+}
