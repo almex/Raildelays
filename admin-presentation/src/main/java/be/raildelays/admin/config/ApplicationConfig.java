c4b58fa3ebdf81c07027d58eec412ece103648fe
Almex <soumagne.alexis@belgacom.net>
2014-01-04 00:00:06 +0100

    Refactored the administration presentation module with Spring Boot and Spring Data Rest

---
+++ admin-presentation/src/main/java/be/raildelays/admin/config/ApplicationConfig.java
@@ -0,0 +1,32 @@
+package be.raildelays.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
+
+/**
+ * @author Almex
+ */
+@ImportResource("classpath:spring/batch/raildelays-batch-integration-context.xml")
+public class ApplicationConfig {
+
+    @Bean
+    PersistenceAnnotationBeanPostProcessor getPersistenceAnnotationBeanPostProcessor() {
+        PersistenceAnnotationBeanPostProcessor bean = new PersistenceAnnotationBeanPostProcessor();
+
+        bean.setDefaultPersistenceUnitName("raildelays-repository");
+
+        return bean;
+    }
+
+    @Bean
+    MappingJackson2HttpMessageConverter converter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+        converter.setObjectMapper(new HibernateAwareObjectMapper());
+
+        return converter;
+    }
+
+}
---
+++ assembly/src/main/resources/startServer.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+
+java -jar ./lib/admin-presentation-1.0.0-SNAPSHOT.jar
+PAUSE
