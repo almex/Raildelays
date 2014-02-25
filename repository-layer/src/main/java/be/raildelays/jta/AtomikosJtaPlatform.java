b62f226642ae485e084606277c23a1dec35bf9e1
Almex <soumagne.alexis@belgacom.net>
2014-02-25 14:05:00 +0100

    - Roll-batcked to non-XA config  - Fixed setup in non-XA config  - Added dependency management for Eclipse-link and Open-JPA

--- repository-layer/src/main/java/be/raildelays/jta/AtomikosJtaPlatform.java
+++ repository-layer/src/main/java/be/raildelays/jta/AtomikosJtaPlatform.java
@@ -1,15 +1,15 @@
 package be.raildelays.jta;
 
-import com.atomikos.icatch.jta.UserTransactionManager;
+/*import com.atomikos.icatch.jta.UserTransactionManager;
 import org.hibernate.engine.transaction.jta.platform.internal.AbstractJtaPlatform;
 
 import javax.transaction.TransactionManager;
-import javax.transaction.UserTransaction;
+import javax.transaction.UserTransaction;*/
 
 /**
  * Created by PCuser on 4/02/14.
  */
-public class AtomikosJtaPlatform extends AbstractJtaPlatform {
+/*public class AtomikosJtaPlatform extends AbstractJtaPlatform {
 
     private static final long serialVersionUID = 1L;
     private UserTransactionManager utm;
@@ -27,4 +27,4 @@ public class AtomikosJtaPlatform extends AbstractJtaPlatform {
     protected UserTransaction locateUserTransaction() {
         return utm;
     }
-}
+}*/
