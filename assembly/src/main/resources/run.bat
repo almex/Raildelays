ee6ec8e6c23de44874e24d61d864e22006091c27
almex <almex@users.noreply.github.com>
2014-07-01 15:26:35 +0200

    Update run.bat
    Remove version from final name of JAR's

--- assembly/src/main/resources/run.bat
+++ assembly/src/main/resources/run.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 
-java -jar -Dlog4j.configurationFile=./conf/log4j2.xml ./lib/raildelays-batch-1.0.0-SNAPSHOT.jar 
+java -jar -Dlog4j.configurationFile=./conf/log4j2.xml ./lib/raildelays-batch.jar 
 PAUSE
