bbb204974138eb34e6b836d3ce854fd677bf3c79
almex <almex@users.noreply.github.com>
2014-07-01 15:28:11 +0200

    Update runDebug.bat
    Removed version from final name of raildelays-batch.jar

--- assembly/src/main/resources/runDebug.bat
+++ assembly/src/main/resources/runDebug.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000 -jar  -Dlog4j.configurationFile=./conf/log4j2.xml ./lib/raildelays-batch-1.0.0-SNAPSHOT.jar 
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000 -jar  -Dlog4j.configurationFile=./conf/log4j2.xml ./lib/raildelays-batch.jar 
 PAUSE
