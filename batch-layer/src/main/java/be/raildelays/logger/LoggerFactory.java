cd147e2232897c54c926d609e55201d0216b423b
almex <almex@users.noreply.github.com>
2014-07-16 15:39:44 +0200

    Update LoggerFactory.java

--- batch-layer/src/main/java/be/raildelays/logger/LoggerFactory.java
+++ batch-layer/src/main/java/be/raildelays/logger/LoggerFactory.java
@@ -8,4 +8,12 @@ public class LoggerFactory {
     public static Logger getLogger(String prefix, Class<?> classe) {
         return new RaildelaysLogger(prefix, org.slf4j.LoggerFactory.getLogger(classe));
     }
+
+    public static Logger getLogger(String prefix, Class<?> classe, char separator) {
+        RaildelaysLogger logger = new RaildelaysLogger(prefix, org.slf4j.LoggerFactory.getLogger(classe));
+
+        logger.setSeparator(separator);
+
+        return logger;
+    }
 }
