0fd7eb5d559e634d7da80a8e049cc2946053e1e3
almex <almex@users.noreply.github.com>
2014-07-22 14:03:57 +0200

    Update run.bat
    Redirect Hibernate logging to slf4j

--- assembly/src/main/resources/run.bat
+++ assembly/src/main/resources/run.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 
-java -jar -Dlog4j.configurationFile=./conf/log4j2.xml ./lib/raildelays-batch.jar 
+java -jar -Dlog4j.configurationFile=./conf/log4j2.xml -Dorg.jboss.logging.provider=slf4j ./lib/raildelays-batch.jar 
 PAUSE
