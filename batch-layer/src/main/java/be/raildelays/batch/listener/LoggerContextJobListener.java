0dc002acab00bfc851fd8ed155049ccd57333e59
almex <almex@users.noreply.github.com>
2014-07-22 13:45:31 +0200

    Update LoggerContextJobListener.java
    [#4] Moved from log4j2 ThreadContext to SLF4J MDC (for more interoperability)

--- batch-layer/src/main/java/be/raildelays/batch/listener/LoggerContextJobListener.java
+++ batch-layer/src/main/java/be/raildelays/batch/listener/LoggerContextJobListener.java
@@ -1,8 +1,11 @@
 package be.raildelays.batch.listener;
 
-import org.apache.logging.log4j.ThreadContext;
+import org.slf4j.MDC;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Add jobExecutionId to {@link org.apache.logging.log4j.ThreadContext} to be able to create
@@ -15,16 +18,20 @@ public class LoggerContextJobListener implements JobExecutionListener {
 
     public static final String JOB_EXECUTION_ID = "jobExecutionId";
     public static final String JOB_INSTANCE_ID = "jobInstanceId";
+    public static final String DATE_PARAMETER = "dateParameter";
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
-        ThreadContext.put(JOB_EXECUTION_ID, jobExecution.getId().toString());
-        ThreadContext.put(JOB_INSTANCE_ID, jobExecution.getJobId().toString());
+        Date date = jobExecution.getJobParameters().getDate("date");
+        MDC.put(JOB_EXECUTION_ID, jobExecution.getId().toString());
+        MDC.put(JOB_INSTANCE_ID, jobExecution.getJobId().toString());
+        MDC.put(DATE_PARAMETER, new SimpleDateFormat("yyyy-MM-dd").format(date));
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        ThreadContext.remove(JOB_EXECUTION_ID);
-        ThreadContext.remove(JOB_INSTANCE_ID);
+        MDC.remove(JOB_EXECUTION_ID);
+        MDC.remove(JOB_INSTANCE_ID);
+        MDC.remove(DATE_PARAMETER);
     }
 }
