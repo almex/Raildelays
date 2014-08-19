e7450757cbda747e8902b06a508733a5512c85fc
almex <almex@users.noreply.github.com>
2014-08-19 12:58:32 +0200

    Update run.sh
    [#15] Configured Log4j2 to enable asynchronous logging

--- assembly/src/main/resources/run.sh
+++ assembly/src/main/resources/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dlog4j.configurationFile=./conf/log4j2.xml -jar ./lib/raildelays-batch.jar
+java -Dlog4j.configurationFile=./conf/log4j2.xml -Dorg.jboss.logging.provider=slf4j -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -jar ./lib/raildelays-batch.jar
