ed5bcec3136d9d39efac0dfb6e27d011339a78d8
almex <alexis.soumagne@gmail.com>
2014-12-18 20:59:01 +0100

    Implemented a better temporary file handling
    This implementation is more reliable and more infrastructure friendly

--- batch-layer/src/main/java/be/raildelays/batch/support/TemporaryFileResourceLocator.java
+++ batch-layer/src/main/java/be/raildelays/batch/support/TemporaryFileResourceLocator.java
@@ -2,50 +2,56 @@ package be.raildelays.batch.support;
 
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.support.ResourceLocator;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.util.Assert;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
- * This class is responsible to build path of a temporary {@link java.io.File} based on  the given
- * {@link org.springframework.core.io.Resource} parent directory and append the {@link #relativePathAndFileName} to it.
- * Then it stores, as a <code>String</code>, the resulting absolute path into the
- * {@link org.springframework.batch.item.ExecutionContext} using the {@link #keyName} .
+ * This class is responsible to build {@link java.nio.file.Path} of a temporary {@code File}
+ * and create it. This file will be destroyed at the shutdown of the JVM.
+ * Then it stores, as a <code>String</code>, the resulting absolute {@link java.nio.file.Path} into the
+ * {@link org.springframework.batch.item.ExecutionContext} using the {@link #keyName}.
  *
  * @author Almex
  * @since 1.2
  */
-//TODO replace this implementation by using Files.createTempFile() and System.getProperty("java.io.tmpdir")
-public class TemporaryFileResourceLocator implements ResourceLocator {
-
-    private Resource resource;
-
-    private String relativePathAndFileName;
+public class TemporaryFileResourceLocator implements ResourceLocator, InitializingBean {
 
     private String keyName;
 
     @Override
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(keyName, "The 'keyName' property must be provided");
+    }
+
+    @Override
     public Resource getResource(ExecutionContext context) throws IOException {
-        Resource result = new FileSystemResource(resource.getFile().getParent() + File.separator
-                + relativePathAndFileName);
+        Path tmpDir = Paths.get(System.getProperty("java.io.tmpdir"));
+        Path tmpFile = Files.createTempFile(tmpDir, "raildelays-", ".xls.tmp");
 
+        /*
+         * To avoid any trouble with 3rd party components, we choose to delete this temporary file
+         * on exit of the JVM. Then we have more guaranty that there is no file handle blocking the deletion.
+         * In other words, if a 3rd party component have a remaining open stream on this file, the JVM guaranty
+         * to close any stream before deleting it otherwise the delete may no be executed by the underlying
+         * file system.
+         */
+        tmpFile.toFile().deleteOnExit();
 
+        context.putString(keyName, tmpFile.toAbsolutePath().toString());
 
-        context.putString(keyName, result.getFile().getAbsolutePath());
-
-        return result;
+        return new FileSystemResource(tmpFile.toFile());
     }
 
     @Override
     public void setResource(Resource resource) {
-        this.resource = resource;
-    }
-
-    public void setRelativePathAndFileName(String relativePathAndFileName) {
-        this.relativePathAndFileName = relativePathAndFileName;
+        //NOOP
     }
 
     public void setKeyName(String keyName) {
