e5da76e8a3cdcc384aa49b6ebe69652c9776b94a
almex <almex@users.noreply.github.com>
2015-04-14 14:38:20 +0200

    [#19] Split before / split after a threshold date

--- batch-layer/src/main/java/be/raildelays/batch/processor/SplitExcelSheetProcessor.java
+++ batch-layer/src/main/java/be/raildelays/batch/processor/SplitExcelSheetProcessor.java
@@ -4,25 +4,50 @@ import be.raildelays.domain.xls.ExcelRow;
 import org.springframework.batch.item.ItemProcessor;
 
 import java.util.Date;
+import java.util.function.Consumer;
 
 /**
-* Created by Almex on 21/12/2014.
+ * We filter items based on a threshold date.
+ * You have the choice to keep what is before or after the threshold date.
+ *
+ * @author Almex
+ * @since 1.2
 */
 public class SplitExcelSheetProcessor implements ItemProcessor<ExcelRow, ExcelRow> {
 
     private Date thresholdDate;
 
+    private Mode mode;
+
+    public static enum Mode {
+        BEFORE, AFTER_OR_EQUALS;
+    }
+
     @Override
     public ExcelRow process(ExcelRow item) throws Exception {
+        ExcelRow result = null;
 
-        if (item.getDate().before(thresholdDate)) {
-            return null;
-        } else {
-            return item;
+        switch (mode) {
+            case BEFORE:
+                if (item.getDate().before(thresholdDate)) {
+                    result = item;
+                }
+                break;
+            case AFTER_OR_EQUALS:
+                if (item.getDate().after(thresholdDate) || item.getDate().equals(thresholdDate)) {
+                    result = item;
+                }
+                break;
         }
+
+        return result;
     }
 
     public void setThresholdDate(Date thresholdDate) {
         this.thresholdDate = thresholdDate;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
     }
 }
