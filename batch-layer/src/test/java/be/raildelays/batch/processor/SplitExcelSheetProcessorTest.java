461ba9dfca4c54853d8f963b993f05b0eefce546
almex <almex@users.noreply.github.com>
2015-04-14 14:39:36 +0200

    [#19] Split before / split after a threshold date

--- batch-layer/src/test/java/be/raildelays/batch/processor/SplitExcelSheetProcessorTest.java
+++ batch-layer/src/test/java/be/raildelays/batch/processor/SplitExcelSheetProcessorTest.java
@@ -25,7 +25,57 @@ public class SplitExcelSheetProcessorTest {
     }
 
     @Test
-    public void testFiltered() throws Exception {
+    public void testFilteredBefore() throws Exception {
+        processor.setMode(SplitExcelSheetProcessor.Mode.BEFORE);
+
+        BatchExcelRow item = new BatchExcelRow
+                .Builder(Date.from(Instant.parse("2014-12-31T00:00:00.00Z")), Sens.DEPARTURE)
+                .build(false);
+        ExcelRow result = processor.process(item);
+
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testNotFilteredBefore() throws Exception {
+        processor.setMode(SplitExcelSheetProcessor.Mode.BEFORE);
+
+        BatchExcelRow item = new BatchExcelRow
+                .Builder(Date.from(Instant.parse("2014-01-01T00:00:00.00Z")), Sens.DEPARTURE)
+                .build(false);
+        ExcelRow result = processor.process(item);
+
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testNotFilteredEquals() throws Exception {
+        processor.setMode(SplitExcelSheetProcessor.Mode.BEFORE);
+
+        BatchExcelRow item = new BatchExcelRow
+                .Builder(Date.from(Instant.parse("2014-01-18T00:00:00.00Z")), Sens.DEPARTURE)
+                .build(false);
+        ExcelRow result = processor.process(item);
+
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testFilteredEquals() throws Exception {
+        processor.setMode(SplitExcelSheetProcessor.Mode.AFTER_OR_EQUALS);
+
+        BatchExcelRow item = new BatchExcelRow
+                .Builder(Date.from(Instant.parse("2014-01-18T00:00:00.00Z")), Sens.DEPARTURE)
+                .build(false);
+        ExcelRow result = processor.process(item);
+
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void testFilteredAfter() throws Exception {
+        processor.setMode(SplitExcelSheetProcessor.Mode.AFTER_OR_EQUALS);
+
         BatchExcelRow item = new BatchExcelRow
                 .Builder(Date.from(Instant.parse("2014-01-01T00:00:00.00Z")), Sens.DEPARTURE)
                 .build(false);
@@ -35,7 +85,9 @@ public class SplitExcelSheetProcessorTest {
     }
 
     @Test
-    public void testNotFiltered() throws Exception {
+    public void testNotFilteredAfter() throws Exception {
+        processor.setMode(SplitExcelSheetProcessor.Mode.AFTER_OR_EQUALS);
+
         BatchExcelRow item = new BatchExcelRow
                 .Builder(Date.from(Instant.parse("2014-12-31T00:00:00.00Z")), Sens.DEPARTURE)
                 .build(false);
