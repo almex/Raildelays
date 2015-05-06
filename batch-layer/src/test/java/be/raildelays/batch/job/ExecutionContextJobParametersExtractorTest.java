a2f1db5122f45ed9de4b81889a41d2067e7dfb99
almex <alexis.soumagne@gmail.com>
2015-05-06 23:29:38 +0200

    - [#19] fixed some path issue and added one test

--- batch-layer/src/test/java/be/raildelays/batch/job/ExecutionContextJobParametersExtractorTest.java
+++ batch-layer/src/test/java/be/raildelays/batch/job/ExecutionContextJobParametersExtractorTest.java
@@ -1,9 +1,15 @@
 package be.raildelays.batch.job;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.SimpleJob;
 import org.springframework.batch.test.MetaDataInstanceFactory;
+
+import java.util.Date;
 
 /**
  * Created by xbmc on 06-05-15.
@@ -19,8 +25,19 @@ public class ExecutionContextJobParametersExtractorTest {
 
     @Test
     public void testGetJobParameters() throws Exception {
-        StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution();
+        JobParametersBuilder builder = new JobParametersBuilder();
 
-        jobParametersExtractor.getJobParameters(null, stepExecution);
+        builder.addString("foo", "bar");
+
+        StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution(builder.toJobParameters());
+
+        stepExecution.getExecutionContext().put("foo2", "bar2");
+        stepExecution.getExecutionContext().put("foo3", 1L);
+        stepExecution.getExecutionContext().put("foo4", 2.2);
+        stepExecution.getExecutionContext().put("foo5", new Date());
+
+        JobParameters jobParameters = jobParametersExtractor.getJobParameters(new SimpleJob("foo"), stepExecution);
+        Assert.assertNotNull(jobParameters);
+        Assert.assertEquals(5, jobParameters.getParameters().size());
     }
 }
