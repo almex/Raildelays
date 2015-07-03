334bbb2ab24d6e22c70d6d53cef6adb27988ab87
Alexis SOUMAGNE <almex@users.noreply.github.com>
2015-07-03 10:56:43 +0200

    Update EnhancedDataFieldMaxValueIncrementerFactory.java

--- batch-layer/src/main/java/org/springframework/batch/item/database/support/EnhancedDataFieldMaxValueIncrementerFactory.java
+++ batch-layer/src/main/java/org/springframework/batch/item/database/support/EnhancedDataFieldMaxValueIncrementerFactory.java
@@ -1,29 +1,7 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Almex
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
+package org.springframework.batch.database.support;
 
-package org.springframework.batch.item.database.support;
-
+import org.springframework.batch.item.database.support.DataFieldMaxValueIncrementerFactory;
+import org.springframework.batch.item.database.support.DefaultDataFieldMaxValueIncrementerFactory;
 import org.springframework.batch.support.DatabaseType;
 import org.springframework.jdbc.support.incrementer.DataFieldMaxValueIncrementer;
 import org.springframework.jdbc.support.incrementer.HsqlSequenceMaxValueIncrementer;
@@ -33,10 +11,11 @@ import javax.sql.DataSource;
 import static org.springframework.batch.support.DatabaseType.HSQL;
 
 /**
- * Allow to use {@link org.springframework.jdbc.support.incrementer.HsqlSequenceMaxValueIncrementer} instead of
- * {@link org.springframework.jdbc.support.incrementer.HsqlMaxValueIncrementer} for HSQLDB.
+ * This implementation prone Sequence based max values opposed to Column based max values used by the
+ * {@link DefaultDataFieldMaxValueIncrementerFactory}. For instance with HSQLDB, this implementation use
+ * {@code HsqlSequenceMaxValueIncrementer} instead of {@code HsqlMaxValueIncrementer}.
  */
-public class EnhancedDataFieldMaxValueIncrementerFactory implements DataFieldMaxValueIncrementerFactory {
+public class DefaultSequenceMaxValueIncrementerFactory implements DataFieldMaxValueIncrementerFactory {
 
     private DefaultDataFieldMaxValueIncrementerFactory delegate;
 
@@ -56,9 +35,10 @@ public class EnhancedDataFieldMaxValueIncrementerFactory implements DataFieldMax
         this.incrementerColumnName = incrementerColumnName;
     }
 
-    public EnhancedDataFieldMaxValueIncrementerFactory(DataSource dataSource) {
+    public DefaultSequenceMaxValueIncrementerFactory(DataSource dataSource) {
         this.dataSource = dataSource;
         delegate = new DefaultDataFieldMaxValueIncrementerFactory(dataSource);
+        delegate.setIncrementerColumnName(incrementerColumnName);
     }
 
     @Override
