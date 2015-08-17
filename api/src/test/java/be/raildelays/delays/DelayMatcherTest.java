3d6e4ce1a67e8f598e24b8ee324aa59d865916f7
Alexis SOUMAGNE <almex@users.noreply.github.com>
2015-08-17 13:37:32 +0200

    Improved DSL API

--- api/src/test/java/be/raildelays/delays/DelayMatcherTest.java
+++ api/src/test/java/be/raildelays/delays/DelayMatcherTest.java
@@ -3,13 +3,26 @@ package be.raildelays.delays;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+
 import static be.raildelays.delays.DelayMatcher.*;
 
 public class DelayMatcherTest {
 
     @Test
     public void differenceBetweenTwoDefaultTimestampDelayIsEqualToZero() {
-        Assert.assertTrue(difference(between(new TimestampDelay(), new TimestampDelay()), is(equalTo(0L))));
-        Assert.assertFalse(difference(between(new TimestampDelay(), new TimestampDelay()), is(equalTo(1L))));
+        final TimestampDelay timestampDelay = new TimestampDelay();
+
+        Assert.assertTrue(difference(between(timestampDelay).and(timestampDelay), is(equalTo(0L))));
+        Assert.assertFalse(difference(between(timestampDelay).and(timestampDelay), is(equalTo(1L))));
+    }
+
+    @Test
+    public void differenceBetweenDateAndTimestampDelayIsEqualToZero() {
+        final Date date = new Date();
+        final TimestampDelay timestampDelay = new TimestampDelay(date);
+
+        Assert.assertTrue(difference(between(timestampDelay).and(date), is(equalTo(0L))));
+        Assert.assertTrue(difference(between(date).and(timestampDelay), is(equalTo(0L))));
     }
 }
