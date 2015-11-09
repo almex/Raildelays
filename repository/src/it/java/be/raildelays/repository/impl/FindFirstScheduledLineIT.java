9aafd0e7115bc18be41b37399618c993ab8234e6
Alexis SOUMAGNE <soumagne.alexis@belgacom.net>
2015-11-09 14:36:33 +0100

    [#2] Improved testing

--- repository/src/it/java/be/raildelays/repository/impl/FindFirstScheduledLineIT.java
+++ repository/src/it/java/be/raildelays/repository/impl/FindFirstScheduledLineIT.java
@@ -1,6 +1,5 @@
 package be.raildelays.repository.impl;
 
-import be.raildelays.delays.Delays;
 import be.raildelays.domain.entities.LineStop;
 import be.raildelays.domain.entities.Station;
 import be.raildelays.domain.entities.Train;
@@ -9,16 +8,10 @@ import com.excilys.ebi.spring.dbunit.config.DBOperation;
 import com.excilys.ebi.spring.dbunit.test.DataSet;
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 
 import javax.annotation.Resource;
 import java.text.ParseException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @DataSet(value = "classpath:FindArrivalDelaysIT.xml",
         tearDownOperation = DBOperation.DELETE_ALL, dataSourceSpringName = "dataSource")
@@ -28,10 +21,17 @@ public class FindFirstScheduledLineIT extends AbstractIT {
     private LineStopDao lineStopDao;
 
     @Test
-    public void testFindArrivalDelaysPageable() throws ParseException {
+    public void testFound() throws ParseException {
         LineStop result = lineStopDao.findFistScheduledLine(new Train("466"), new Station("Bruxelles-Central"));
 
         Assert.assertEquals(LocalDate.of(2000, 1, 1), result.getDate());
+    }
+
+    @Test
+    public void testNotFound() throws ParseException {
+        LineStop result = lineStopDao.findFistScheduledLine(new Train("555"), new Station("Bruxelles-Central"));
+
+        Assert.assertNull(result);
     }
 
 
