754e5f7377f6d2f850edc5fc9d06150bfc3db1d7
Alexis SOUMAGNE <alexis.soumagne@gmail.com>
2016-01-24 19:44:10 +0100

    [#60]  Improved testing

--- domain/src/test/java/be/raildelays/domain/xls/ExcelRowTest.java
+++ domain/src/test/java/be/raildelays/domain/xls/ExcelRowTest.java
@@ -4,10 +4,17 @@ import be.raildelays.domain.Sens;
 import be.raildelays.domain.entities.Station;
 import be.raildelays.domain.entities.TrainLine;
 import com.github.almex.pojounit.AbstractObjectTest;
+import org.junit.Test;
 import org.junit.experimental.theories.DataPoint;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
 
 public class ExcelRowTest extends AbstractObjectTest {
 
@@ -58,4 +65,20 @@ public class ExcelRowTest extends AbstractObjectTest {
                 .effectiveTrain1(new TrainLine.Builder(515L).build())
                 .build();
     }
+
+    @Test
+    public void testCompareToEquals() {
+        assertThat(DATA_POINT1.compareTo(DATA_POINT2), is(equalTo(0)));
+    }
+
+    @Test
+    public void testCompareToGreater() {
+        assertThat(DATA_POINT4.compareTo(DATA_POINT1), is(greaterThan(0)));
+    }
+
+    @Test
+    public void testCompareToLess() {
+        assertThat(DATA_POINT1.compareTo(DATA_POINT4), is(lessThan(0)));
+    }
+
 }
